e913df14022b7f1d1b07af7327864e4129491ef8
ericzhuyilin <yiz095@ucsd.edu>
2022-08-11 16:21:57 -0700

    hash function implementation

--- hash.c
+++ hash.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlb.h>
+#include <string.h>
+#include <pa1.h>
 
 unsigned int hash(char* str){
     unsigned int hashVal = HASH_START_VAL;
-    int len = strlen(*str);
+    int len = strlen(str);
 
+    for(int i = 0; i <= len; i+=2){
+        hashVal = hashVal*HASH_PRIME + (int)str[i];	
+    }
 
+    for(int i = 0; i <= len; i+=2){
+        hashVal = hashVal*HASH_PRIME + (int)str[i];	
+    }
+
+    return hashVal;
 }
 

d543489402f7191b9aa885e745ee7a0a418337ac
ericzhuyilin <yiz095@ucsd.edu>
2022-08-16 13:55:21 -0700

    testpopulateTable.c created

---
+++ testpopulateTable.c
@@ -0,0 +1,36 @@
+/*
+ * Filename: testpopulateTable
+ * Author: Yilin Zhu
+ * Userid: A15862322
+ * Description: Unit test program to test the function hash().
+ * Date: 2022 8.16
+ * Sources of Help: Lecture slides, C documentation.
+ */
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "test.h"    /* For TEST() macro and stdio.h */
+#include "pa1.h"
+
+void testpopulateTable() {
+
+  // My tests
+  
+
+}
+
+/* 
+ * Function Name: main()
+ * Function prototype: int main();
+ * Description: The test driver. Runs specified tests.
+ * Side Effects: None
+ * Error Conditions: None
+ * Return Value: 0 on exit success.
+ */
+int main() {
+  fprintf( stderr, "Testing hash...\n\n" );
+  testpopulateTable();
+  fprintf( stderr, "\nDone running tests.\n" );
+  return 0;
+}

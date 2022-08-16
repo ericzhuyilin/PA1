9b416581401a60ccc69ffa4f64f68fdfbe0adbbd
ericzhuyilin <yiz095@ucsd.edu>
2022-08-16 15:15:37 -0700

    checkTable modified

--- checkTable.c
+++ checkTable.c
@@ -4,16 +4,15 @@
 #include "pa1.h"
 
 int checkTable(char* str, linkedListNode_t** hashtbl){
-    linkedListNode_t *bucket = retrieveLinkedList(hashtbl, str);
-    if(*bucket == NULL){
+    linkedListNode_t *curr = retrieveLinkedList(hashtbl, str);
+    if(curr == NULL){
         return 0;
     }
-    linkedListNode_t *curr = **bucket;
     while(curr != NULL){
-        if(strcmp(str, curr.value) == 0){
+        if(strcmp(str, (*curr).value) == 0){
             return 1;
 	}
-	curr = curr.next;
+	curr = (*curr).next;
     }
     return 0;
 }
